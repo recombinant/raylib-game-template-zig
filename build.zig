15bb39c6222518b50ac5d3dad64b955406b8bdfe
recombinant <gituser@recombinant.me.uk>
2024-02-29 22:44:46 +0000

    tweak: remove an extraneous blank line

--- build.zig
+++ build.zig
@@ -3,7 +3,6 @@ const raylib_build = @import("raylib/src/build.zig");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
